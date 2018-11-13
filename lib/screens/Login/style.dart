9aa43edc215be9e61f1c528ffa570c93af4bed17
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-11-13 08:24:05 +0000

    removed exact prefix from assetimage

--- lib/screens/Login/style.dart
+++ lib/screens/Login/style.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 
 DecorationImage backgroundImage = new DecorationImage(
-  image: new ExactAssetImage('assets/login-screen-background.png'),
+  image: new AssetImage('assets/login-screen-background.png'),
   fit: BoxFit.cover,
 );
 
-ExactAssetImage logo = new ExactAssetImage("assets/logo.png");
+AssetImage logo = new AssetImage("assets/logo.png");
 
 
 
